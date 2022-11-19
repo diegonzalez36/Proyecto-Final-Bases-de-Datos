be4b9dd2691a5186c6cdf94a26153edc8c0396b8
Your Name <example@example.com>
2022-11-19 18:47:28 -0500

    Añadida clase database y metodos

--- db.py
+++ db.py
@@ -6,4 +6,31 @@ class Database:
         self.con = sqlite3.connect(db)
         self.cur = self.con.cursor()
         self.cur.execute("CREATE TABLE IF NOT EXISTS Productos (id INTEGER PRIMARY KEY, producto text, cliente text, tipo text, precio text)")
-        self.con.commit() 
+        self.con.commit()
+
+    def capturar(self):
+        self.cur.execute("SELECT * FROM Productos")
+        filas = self.cur.fetchall()
+        return filas
+
+    def insertar(self, producto, cliente, tipo, precio):
+        self.cur.execute("INSERT INTO Productos VALUES (NULL, ?, ?, ?, ?)", (producto, cliente, tipo, precio))    
+        self.con.commit()
+
+    def remover(self, id):
+        self.cur.execute("DELETE FROM Productos WHERE id=?", (id,))
+        self.con.commit()
+    
+    def actualizar(self, id, producto, cliente, tipo, precio):
+        self.cur.execute("UPDATE Productos SET producto = ?, cliente=?, tipo=?, precio=? WHERE id =?", (producto, cliente, tipo, precio, id))
+        self.con.commit()
+
+    def __del__(self):
+        self.con.close()
+        
+""""
+db = Database('tienda.db')
+db.insertar("Sopa", "Diego", "Fuerte", "20000")
+db.insertar("Arroz", "Andres", "Entrada", "9000")
+db.insertar("Pollo", "Carlos", "Entrada", "7000")
+"""
